bb007a1ef5692c0e30d8343b20d17b93ab7596f5
Leonid Kondrashov <leo.kondrashov@gmail.com>
2025-10-13 06:05:35 -0600

    fix cpu template
    Signed-off-by: Leonid Kondrashov <leo.kondrashov@gmail.com>

--- runtime/service.go
+++ runtime/service.go
@@ -593,11 +593,11 @@ func (s *service) createVM(requestCtx context.Context, request *proto.CreateVMRe
 	if request.LoadSnapshot {
 		if request.SnapshotPath == "" || request.ContainerSnapshotPath == "" {
 			return errors.New("failed to load snapshot: snapshot path or container snapshot path was not provided")
-		} 
+		}
 		if request.MemFilePath == "" && request.MemBackend.BackendType == "" {
 			return errors.New("either mem_file_path or mem_backend should be provided")
 		}
-		
+
 		opts = append(opts,
 			firecracker.WithSnapshot(
 				request.MemFilePath,
